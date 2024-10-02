00186d99468691f83ccd293ddd685ba2e032b76d
Ferref <kovacsferenc026@gmail.com>
2024-10-02 18:49:27 +0200

    Base flutter project

---
+++ README.md
@@ -0,0 +1,3 @@
+# ebook_reader_mobilapp
+
+Read ebook and pdf files.
---
+++ android/app/src/main/kotlin/com/example/ebook_reader_mobilapp/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.ebook_reader_mobilapp
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
