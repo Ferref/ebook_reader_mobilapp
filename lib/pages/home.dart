7bc8bf6369bae691e1a51678e667716623472564
Ferref <kovacsferenc026@gmail.com>
2024-10-07 16:25:37 +0200

    HomePage with AppBar

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -1,29 +1,74 @@
+import 'package:ebook_reader_mobilapp/themes/app_colors.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+
+class CustomAppBar extends StatelessWidget implements PreferredSizeWidget {
+  @override
+  final Size preferredSize; // Required for PreferredSizeWidget
+
+  const CustomAppBar({super.key})
+      : preferredSize = const Size.fromHeight(kToolbarHeight);
+
+  @override
+  Widget build(BuildContext context) {
+    return AppBar(
+      title: const Text(
+        'Ebook-HomePage',
+        style: TextStyle(
+          color: AppColors.text,
+          fontSize: 20,
+          fontWeight: FontWeight.bold,
+        ),
+      ),
+      backgroundColor: AppColors.background,
+      elevation: 0.0,
+      centerTitle: true,
+      leading: GestureDetector(
+        onTap: () {
+          // Add your onTap functionality here
+        },
+        child: Container(
+          margin: const EdgeInsets.all(10),
+          alignment: Alignment.center,
+          decoration: BoxDecoration(
+            color: Colors.black,
+            borderRadius: BorderRadius.circular(10),
+          ),
+          child: SvgPicture.asset(
+            'assets/icons/books.svg',
+            height: 20,
+            width: 20,
+          ),
+        ),
+      ),
+      actions: [
+        Container(
+          margin: const EdgeInsets.all(10),
+          alignment: Alignment.center,
+          decoration: BoxDecoration(
+            color: Colors.black,
+            borderRadius: BorderRadius.circular(10),
+          ),
+          child: SvgPicture.asset(
+            'assets/icons/books.svg',
+            height: 20,
+            width: 20,
+          ),
+        ),
+      ],
+    );
+  }
+}
 
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text(
-          'Ebook-HomePage',
-          style: TextStyle(
-            color: Colors.black,
-            fontSize: 20,
-            fontWeight: FontWeight.bold,
-          ),
-        ),
-        backgroundColor: Colors.white,
-        elevation: 0.0,
-        centerTitle: true,
-        leading: Container(
-            margin: const EdgeInsets.all(10),
-            decoration: BoxDecoration(
-              color: Colors.black,
-              borderRadius: BorderRadius.circular(10),
-            )),
+    return const Scaffold(
+      appBar: CustomAppBar(),
+      body: Center(
+        child: Text('HámPédzs'),
       ),
     );
   }
---
+++ lib/themes/app_colors.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const Color background = Color(0xFF121212); // Dark Gray
+  static const Color text = Color(0xFFE0E0E0); // Light Gray
+  static const Color highlight = Color(0xFFFFAB00); // Amber
+  static const Color accent = Color(0xFFBB86FC); // Purple
+  static const Color secondaryBackground = Color(0xFF1E1E1E); // Dark Gray
+  static const Color footerHeader = Color(0xFF3700B3); // Deep Purple
+}
