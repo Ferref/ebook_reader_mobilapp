513e60c54c5c7152c7e878ae3beeda262759fbe0
Ferref <kovacsferenc026@gmail.com>
2024-10-29 20:03:16 +0100

    Ebook, pdf, filepicker dependencia hozzaadva

--- lib/services/book_loader.dart
+++ lib/services/book_loader.dart
@@ -1,16 +1,27 @@
-import 'package:flutter/material.dart';
 import 'package:file_picker/file_picker.dart';
+import 'package:flutter/material.dart';
 import 'package:epub_view/epub_view.dart';
 import 'package:flutter_pdfview/flutter_pdfview.dart';
 import 'dart:io';
 
 class BookLoader {
   Future<void> loadBook(BuildContext context) async {
-    FilePickerResult? result = await FilePicker.platform.pickFiles(
-        type: FileType.custom, allowedExtensions: ['epub', 'pdf', 'prc']);
+    final result = await FilePicker.platform.pickFiles(
+      type: FileType.custom,
+      allowedExtensions: ['epub', 'pdf', 'prc'],
+    );
 
     if (result != null) {
-      String? filePath = result.files.single.path;
+      final file = result.files.first;
+      final filePath = file.path!;
+
+      if (file.extension == 'epub') {
+        _openEpubReader(context, filePath);
+      } else if (file.extension == 'pdf') {
+        _openReader(context, filePath);
+      } else {
+        _showUnsupportedFileDialog(context);
+      }
     }
   }
 }
